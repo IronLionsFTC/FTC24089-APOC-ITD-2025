749332daeaba8b1e11e777667eb2e081e5425f35
Harrison <hchap1@hotmail.com>
2025-03-17 22:34:26 +1000

    feature: Limelight interface java class created

--- TeamCode/src/main/java/core/computerVision/Limelight.java
+++ TeamCode/src/main/java/core/computerVision/Limelight.java
@@ -4,6 +4,8 @@ import com.qualcomm.hardware.limelightvision.LLResult;
 import com.qualcomm.hardware.limelightvision.Limelight3A;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
+import core.math.Vector;
+
 public class Limelight {
     private final Limelight3A hardware;
 
@@ -15,8 +17,28 @@ public class Limelight {
     public void disable() { hardware.stop(); }
     public void shutdown() { hardware.shutdown(); }
 
-    public void query() {
+    public class SampleState {
+        public double angle;
+        public Vector center;
+
+        public SampleState(double angle, Vector center) {
+            this.angle = angle;
+            this.center = center;
+        }
+    }
+
+    public SampleState query() {
         LLResult result = hardware.getLatestResult();
-        result.getPythonOutput();
+
+        if (result == null) return null;
+
+        double[] result_array = result.getPythonOutput();
+        double angle = result_array[0];
+
+        // This is POSSIBLE, but so unlikely it can be treated as an error
+        if (angle == 0) return null;
+
+        Vector center = Vector.cartesian(result_array[1], result_array[2]);
+        return new SampleState(angle, center);
     }
 }
