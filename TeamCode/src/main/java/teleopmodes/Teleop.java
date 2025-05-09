ffbdc67fd3e6f44299b0b50cafc88d9d4fb5b761
hchap1 <hchap1@hotmail.com>
2025-05-09 20:09:00 +1000

    teleop comments

--- TeamCode/src/main/java/teleopmodes/Teleop.java
+++ TeamCode/src/main/java/teleopmodes/Teleop.java
@@ -1,5 +1,6 @@
 package teleopmodes;
 
+// Package imports from FTC libaries
 import com.acmerobotics.dashboard.FtcDashboard;
 import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
 import com.arcrobotics.ftclib.command.CommandOpMode;
@@ -10,6 +11,7 @@ import com.pedropathing.follower.Follower;
 import com.pedropathing.util.Constants;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
+// Packages from within TeamCode
 import core.computerVision.Limelight;
 import core.controls.Controls.Buttons;
 import core.math.Vector;
@@ -23,21 +25,28 @@ import pedroPathing.constants.LConstants;
 @TeleOp(name = "Teleop", group = "Competition")
 public class Teleop extends CommandOpMode {
 
+    // Create subsystems
     private Drivebase drivebaseSubsystem;
     private Intake intakeSubsystem;
     private Outtake outtakeSubsystem;
     private Buttons buttons;
     private Follower follower;
 
+    // CV handler and buffer for results
     private Limelight limelight;
     private Limelight.SampleState sampleState;
 
     @Override
     public void initialize() {
+
+        // Load pedro tune
         Constants.setConstants(FConstants.class, LConstants.class);
+
+        // initialize follower
         this.follower = new Follower(hardwareMap);
         this.follower.setStartingPose(Vector.cartesian(0, 0).pose(0));
 
+        // Intialize the rest of subsystems
         this.telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
         this.buttons = new Buttons(gamepad1, gamepad2);
         this.intakeSubsystem = new Intake(hardwareMap, this.telemetry);
@@ -55,12 +64,11 @@ public class Teleop extends CommandOpMode {
         // Link buttons to commands
         buttons.intakeCycle.whenPressed(CMD.teleopIntakeCycle(intakeSubsystem));
         buttons.outtakeCycle.whenPressed(CMD.teleopOuttakeCycle(outtakeSubsystem));
-
         buttons.rotateRight.whenPressed(CMD.rotateCW(drivebaseSubsystem));
         buttons.rotateLeft.whenPressed(CMD.rotateCCW(drivebaseSubsystem));
-
         buttons.override.whenPressed(CMD.teleopOverride(intakeSubsystem, outtakeSubsystem));
 
+        // Spawn a command sequence to rotate the claw to align with a sample
         buttons.useCV.whenPressed(
             CMD.resetCV(sampleState).andThen(
                 CMD.scanForSample(limelight, sampleState, telemetry, follower, intakeSubsystem, false).andThen(
@@ -74,9 +82,32 @@ public class Teleop extends CommandOpMode {
                 CMD.sleepUntil(this::opModeIsActive),
                 new RunCommand(telemetry::update),
                 new ParallelCommandGroup(
+                        // Switch between follower and manual control (follower only used to hold points against collision)
+                        // This means if we get hit when stationary it will correct back when enabled
+                        // could be used to do more CV things in future
                         CMD.constantlyUpdateFollower(follower, drivebaseSubsystem),
+
+                        // Constantly grab the joystick position, scale it like so:
+                        /*
+                                            *
+                         S                  *
+                         p                 *
+                         e                *
+                         e             **
+                         d          ***
+                             *****
+                             Joystick Position
+                         */
+
+                        // This makes it easier to do small movement with little joystick motion, but full speed still is full speed (1^2 = 1, 0.5^2 = 0.25)
                         CMD.setDriveVector(drivebaseSubsystem, intakeSubsystem, buttons.driveX, buttons.driveY, buttons.yaw),
+
+                        // Rotate the claw when in certain states. This command is a wrapper for a bunch of smaller commands
                         CMD.rotateIntakeClaw(intakeSubsystem, buttons.rotateClawRight, buttons.rotateClawLeft),
+
+                        // Optionally scheduled command that uses proximity sensors and encoders to confidently attempt transfer automatically
+                        // If this is not scheduled driver is in control. During comp, if something fails and we can't fix it in time, this could
+                        // be removed so that driver can control it. (IE if outtake sensor breaks or cable breaks, or physical alignment is changed)
                         CMD.teleopAutomaticTransfer(intakeSubsystem, outtakeSubsystem)
                 )
         );
